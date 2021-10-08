6a8ba71afcdbc62171b6f893f94cd70627993aef
snljty <snljty@gmail.com>
2021-10-08 20:58:24 +0800

    add GPU support

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # list_node_info.sh
-## List basis information of main board, CPU, memory and disk of a node.
+## List basis information of main board, CPU, memory, disk and NVidia GPU (if any) on a node.
 Run this script on a local node, or use ssh like `ssh root@mynode1 /path/to/list_node_info.sh` for a remote node.
 Give executable access to this script before running it.
 
--- list_node_info.sh
+++ list_node_info.sh
@@ -17,11 +17,14 @@ echo "Ways: ${num_ways}"
 echo "Cores per way: ${num_cores_per_way}"
 echo "Threads per core: ${num_threads_per_core}"
 echo "Total cores: ${num_total_cores}"
-[[ ${num_threads_per_core} -eq 1 ]] && echo 'Is hyper-threading supported: NO' || echo 'Is hyper-threading supported: YES'
+echo -n 'Is hyper-threading supported: '
+[[ ${num_threads_per_core} -eq 1 ]] && echo 'NO' || echo 'YES'
+which nvidia-smi 2> /dev/null && echo 'GPU:' && nvidia-smi -L | awk -F '(' '{print $1}'
+[[ `whoami` != 'root' ]] && echo 'For other information, please run as root.' && exit
 echo 'Memory:'
 dmidecode -t memory 2> /dev/null | grep -E -A 12 'Memory\s+Device' | grep -E 'Size|Speed' | awk -F ':' '{print $2}' | sed -n 'N;s/\n/\t/p'
 echo 'Disk:'
-fdisk -l 2> /dev/null | grep '^Disk /dev/sd'
+fdisk -l 2> /dev/null | grep '^Disk /dev/' | sort -k 1,1
 # df -lhP | head -n 1
 # df -lhHP --total | tail -n 1
 echo
